32887c8fa832046f450c89af66e4c412b9349fc9
Shalil Awaley <shalil.awaley@readytech.io>
2023-09-21 09:25:51 +0800

    js works

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link d3.v3.min.js
-
+//= link_directory ../javascripts .js
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'chatgpt/chat_request'
   get 'welcome/index'
   root 'welcome#index'
+  resources :generator, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
